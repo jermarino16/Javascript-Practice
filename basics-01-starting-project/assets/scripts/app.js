1661622bd65b233b254a1c18fbc423859e10ae5b
Jeremy Marino <jermarino16@gmail.com>
2020-07-31 21:05:49 -0700

    simple calculator script with functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,26 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10;
+function add() {
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, "");
+}
 
-outputResult(currentResult, "");
+function subtract() {
+  currentResult = currentResult - parseInt(userInput.value);
+  outputResult(currentResult, "");
+}
+
+function multiply() {
+  currentResult = currentResult * parseInt(userInput.value);
+  outputResult(currentResult, "");
+}
+function divide() {
+  currentResult = currentResult / parseInt(userInput.value);
+  outputResult(currentResult, "");
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
