da39d2eac94af7d0459008eb4f63654373b2427a
Jeremy Marino <jermarino16@gmail.com>
2020-08-05 11:26:09 -0700

    toggle backdrop and modal

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,9 +1,26 @@
 const startAddMovieButton = document.querySelector("header button");
 const addMovieModal = document.getElementById("add-modal");
 const deleteMovieModal = document.getElementById("delete-modal");
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
 
 const toggleMovieModal = () => {
-  addMovieModal.classList.add("visible");
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovie);
