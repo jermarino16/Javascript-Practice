780d84a3463d87f465bef8032560c4b55e423751
Jeremy Marino <jermarino16@gmail.com>
2020-08-06 08:18:04 -0700

    find and findIndex examples

--- arrays-02-creating-arrays/app.js
+++ arrays-02-creating-arrays/app.js
@@ -54,3 +54,17 @@ console.log(testResults.indexOf(1.5));
 
 const personData = [{ name: "Max" }, { name: "Manuel" }];
 console.log(personData.indexOf({ name: "Manuel" }));
+
+const manuel = personData.find((person, idx, persons) => {
+  return person.name === "Manuel";
+});
+
+manuel.name = "Anna";
+
+console.log(manuel, personData);
+
+const maxIndex = personData.findIndex((person, idx, persons) => {
+  return person.name === "Max";
+});
+
+console.log(maxIndex);
