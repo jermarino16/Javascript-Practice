81bdc27007e1161bddf9f6c8115788a719cb411e
Jeremy Marino <jermarino16@gmail.com>
2020-08-06 08:36:59 -0700

    spread operator

--- arrays-02-creating-arrays/app.js
+++ arrays-02-creating-arrays/app.js
@@ -131,6 +131,26 @@ const transformedData = data.split(";");
 transformedData[1] = +transformedData[1];
 console.log(transformedData);
 
-const nameFragements = ["Max", "Schwarz"];
-const name = nameFragements.join(" ");
+const nameFragments = ["Max", "Schwarz"];
+const name = nameFragments.join(" ");
 console.log(name);
+
+const copiedNameFragments = [...nameFragments];
+nameFragments.push("Mr");
+console.log(nameFragments, copiedNameFragments);
+
+console.log(Math.min(...prices));
+
+const persons = [
+  { name: "Max", age: 30 },
+  { name: "Manuel", age: 31 },
+];
+const copiedPersons = persons.map((person) => ({
+  name: person.name,
+  age: person.age,
+}));
+
+persons.push({ name: "Anna", age: 29 });
+persons[0].age = 31;
+
+console.log(persons, copiedPersons);
