9c6010e0190fd5e3c7c66aeab669e577f6d9f8e9
Jeremy Marino <jermarino16@gmail.com>
2020-08-06 08:20:19 -0700

    forEach example

--- arrays-02-creating-arrays/app.js
+++ arrays-02-creating-arrays/app.js
@@ -43,28 +43,46 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-// const storedResults = testResults.slice(2);
-const storedResults = testResults.concat([3.99, 2]);
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// // const storedResults = testResults.slice(2);
+// const storedResults = testResults.concat([3.99, 2]);
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
-console.log(testResults.indexOf(1.5));
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(1.5));
 
-const personData = [{ name: "Max" }, { name: "Manuel" }];
-console.log(personData.indexOf({ name: "Manuel" }));
+// console.log(testResults.includes(10.99));
+// console.log(testResults.indexOf(10.99) !== -1);
 
-const manuel = personData.find((person, idx, persons) => {
-  return person.name === "Manuel";
+// const personData = [{ name: 'Max' }, { name: 'Manuel' }];
+// console.log(personData.indexOf({ name: 'Manuel' }));
+
+// const manuel = personData.find((person, idx, persons) => {
+//   return person.name === 'Manuel';
+// });
+
+// manuel.name = 'Anna';
+
+// console.log(manuel, personData);
+
+// const maxIndex = personData.findIndex((person, idx, persons) => {
+//   return person.name === 'Max';
+// });
+
+// console.log(maxIndex);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// for (const price of prices) {
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+  taxAdjustedPrices.push(priceObj);
 });
 
-manuel.name = "Anna";
-
-console.log(manuel, personData);
-
-const maxIndex = personData.findIndex((person, idx, persons) => {
-  return person.name === "Max";
-});
-
-console.log(maxIndex);
+console.log(taxAdjustedPrices);
